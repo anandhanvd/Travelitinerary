f59b3be613e2a0e1b962b86f7fb4dbc13f8ce53f
TONG GIA KHANH 2203548I <2203548I@student.tp.edu.sg>
2024-01-17 11:09:03 +0800

    created Endpoint.cs

---
+++ Travelitinerary/Client/Static/Endpoints.cs
@@ -0,0 +1,18 @@
+﻿namespace Travelitinerary.Client.Static
+{
+    public static class Endpoints
+    {
+        private static readonly string Prefix = "api";
+
+        private static readonly string CustomerEndpoint = $"{Prefix}/customers";
+        private static readonly string BookingEndpoint = $"{Prefix}/bookings";
+        private static readonly string StaffEndpoint = $"{Prefix}/staffs";
+        private static readonly string ItineraryEndpoint = $"{Prefix}/itineraries";
+        private static readonly string HotelEndpoint = $"{Prefix}/hotels";
+        private static readonly string FlightEndpoint = $"{Prefix}/flights";
+        private static readonly string ItineraryActivityEndpoint = $"{Prefix}/itineraryactivies";
+        private static readonly string ActivityEndpoint = $"{Prefix}/activies";
+
+
+    }
+}
