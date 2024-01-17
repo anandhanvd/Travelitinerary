58edcfb9b359eee0ba3e7a8142e5497a2c6a8371
TONG GIA KHANH 2203548I <2203548I@student.tp.edu.sg>
2024-01-17 12:11:28 +0800

    Revert "Fix Endpoint"
    This reverts commit 43baf975848cddc1c2ae70ab3283650dd84893e3.

--- Travelitinerary/Client/Static/Endpoints.cs
+++ Travelitinerary/Client/Static/Endpoints.cs
@@ -1,22 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Travelitinerary.Client.Static
+﻿namespace Travelitinerary.Client.Static
 {
     public static class Endpoints
     {
         private static readonly string Prefix = "api";
 
-        public static readonly string CustomerEndpoint = $"{Prefix}/customers";
-        public static readonly string BookingEndpoint = $"{Prefix}/bookings";
-        public static readonly string StaffEndpoint = $"{Prefix}/staffs";
-        public static readonly string ItineraryEndpoint = $"{Prefix}/itineraries";
-        public static readonly string HotelEndpoint = $"{Prefix}/hotels";
-        public static readonly string FlightEndpoint = $"{Prefix}/flights";
-        public static readonly string ItineraryActivityEndpoint = $"{Prefix}/itineraryactivies";
-        public static readonly string ActivityEndpoint = $"{Prefix}/activies";
+        private static readonly string CustomerEndpoint = $"{Prefix}/customers";
+        private static readonly string BookingEndpoint = $"{Prefix}/bookings";
+        private static readonly string StaffEndpoint = $"{Prefix}/staffs";
+        private static readonly string ItineraryEndpoint = $"{Prefix}/itineraries";
+        private static readonly string HotelEndpoint = $"{Prefix}/hotels";
+        private static readonly string FlightEndpoint = $"{Prefix}/flights";
+        private static readonly string ItineraryActivityEndpoint = $"{Prefix}/itineraryactivies";
+        private static readonly string ActivityEndpoint = $"{Prefix}/activies";
 
 
     }
