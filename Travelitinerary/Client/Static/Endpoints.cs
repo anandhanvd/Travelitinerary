8312aefb796a2a0c33668e5d65fe65c270cf6d98
ANANDHAN VALIYAPARAMBIL DEEPESH 2200135H <2200135H@student.tp.edu.sg>
2024-01-17 12:10:18 +0800

    seed

--- Travelitinerary/Client/Static/Endpoints.cs
+++ Travelitinerary/Client/Static/Endpoints.cs
@@ -4,8 +4,8 @@
     {
         private static readonly string Prefix = "api";
 
-        private static readonly string CustomerEndpoint = $"{Prefix}/customers";
-        private static readonly string BookingEndpoint = $"{Prefix}/bookings";
+        private static readonly string CustomersEndpoint = $"{Prefix}/customers";
+        private static readonly string BookingsEndpoint = $"{Prefix}/bookings";
         private static readonly string StaffEndpoint = $"{Prefix}/staffs";
         private static readonly string ItineraryEndpoint = $"{Prefix}/itineraries";
         private static readonly string HotelEndpoint = $"{Prefix}/hotels";
